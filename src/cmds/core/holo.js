a72827f8960416cf70e6701fe029fca5e935b3bc
unknown <danedjackson@yahoo.com>
2023-04-27 09:53:07 -0500

    Accepting options for slash command

--- src/cmds/core/holo.js
+++ src/cmds/core/holo.js
@@ -1,11 +1,30 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { holoBattles } = require('../../functions/holoBattles');
 
 module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('holo')
-		.setDescription('Replies with Pong!'),
+		.setDescription('Replies with Pong!')
+		.addStringOption(lead => 
+			lead.setName('lead')
+				.setDescription('Holobattle team\'s lead esper')
+				.setRequired(true))
+		.addStringOption(firstMember =>
+			firstMember.setName('first-member')
+						.setDescription('First member esper in holobattle team')
+						.setRequired(true))
+		.addStringOption(secondMember =>
+			secondMember.setName('second-member')
+						.setDescription('Second member esper in holobattle team')
+						.setRequired(true)),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const leadEsper = interaction.options.getString('lead');
+		const firstEsper = interaction.options.getString('first-member');
+		const secondEsper = interaction.options.getString('second-member');
+		
+		//holoBattles()
+
+		await interaction.reply(`${leadEsper} | ${firstEsper} | ${secondEsper}`);
 	},
 };
