bb973700fc3e62313c3c23aa6f60992381fd250e
unknown <danedjackson@yahoo.com>
2023-04-27 13:06:38 -0500

    Processing multiple replies

--- src/cmds/core/holo.js
+++ src/cmds/core/holo.js
@@ -24,21 +24,25 @@ module.exports = {
 		const secondEsper = interaction.options.getString('second-member');
 		
 		const response = getHoloCounters(leadEsper, firstEsper, secondEsper);
-
-		const embed = new EmbedBuilder()
-						.setColor(0x0099FF)
-						.setTitle('Holobattle Formation Recommendations')
-						.addFields(
-							{ name: response[0].esper1.name, value: response[0].esper1.details, inline: true },
-							{ name: response[0].esper2.name, value: response[0].esper2.details, inline: true },
-							{ name: response[0].esper3.name, value: response[0].esper3.details, inline: true },
-						)
-						.setURL('https://playdislyte.com/')
-		const esper1 = new EmbedBuilder().setURL('https://playdislyte.com/').setImage('https://static.wikia.nocookie.net/dislyte/images/b/ba/Jin_Yuyao_sprite.png/revision/latest?cb=20220321180344')
-		const esper2 = new EmbedBuilder().setURL('https://playdislyte.com/').setImage('https://static.wikia.nocookie.net/dislyte/images/5/5e/Dislyte_Everett_2nd_album.jpg/revision/latest/scale-to-width-down/250?cb=20230108073112');
-		const esper3 = new EmbedBuilder().setURL('https://playdislyte.com/').setImage('https://pillarofgaming.com/wp-content/uploads/2021/12/Sif-Sally.jpg');
-						
-
-		await interaction.reply( {embeds: [embed, esper1, esper2, esper3]} );
+		for (let x = 0; x < response.length; x++){
+			const embed = new EmbedBuilder()
+							.setColor(0x0099FF)
+							.setTitle('Holobattle Formation Recommendations')
+							.addFields(
+								{ name: response[x].esper1.name, value: response[x].esper1.details, inline: true },
+								{ name: response[x].esper2.name, value: response[x].esper2.details, inline: true },
+								{ name: response[x].esper3.name, value: response[x].esper3.details, inline: true },
+							)
+							.setURL('https://playdislyte.com/')
+			const esper1 = new EmbedBuilder().setURL('https://playdislyte.com/').setImage('https://static.wikia.nocookie.net/dislyte/images/b/ba/Jin_Yuyao_sprite.png/revision/latest?cb=20220321180344')
+			const esper2 = new EmbedBuilder().setURL('https://playdislyte.com/').setImage('https://static.wikia.nocookie.net/dislyte/images/5/5e/Dislyte_Everett_2nd_album.jpg/revision/latest/scale-to-width-down/250?cb=20230108073112');
+			const esper3 = new EmbedBuilder().setURL('https://playdislyte.com/').setImage('https://pillarofgaming.com/wp-content/uploads/2021/12/Sif-Sally.jpg');
+							
+			if(x > 0) {
+				await interaction.channel.send({embeds: [embed, esper1, esper2, esper3]})
+			} else {
+				await interaction.reply( {embeds: [embed, esper1, esper2, esper3]} );
+			}
+		}
 	},
 };
