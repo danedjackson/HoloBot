5b5b69e34bb94102f027bcf190b12df78d70ea65
unknown <danedjackson@yahoo.com>
2023-04-27 07:47:44 -0500

    Removing reload command for now

--- src/cmds/utility/server.js
+++ src/cmds/utility/server.js
@@ -2,9 +2,10 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
+		.setName('server')
+		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		// interaction.guild is the object representing the Guild in which the command was run
+		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
 	},
 };
